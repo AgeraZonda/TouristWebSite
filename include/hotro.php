90ac81a3e18f792571e68e5b88002c1c1a7e5844
AgeraZonda <nguyenquocduy1001@gmail.com>
2018-01-17 17:18:00 +0700

    update1

--- include/hotro.php
+++ include/hotro.php
@@ -5,12 +5,12 @@
   <tr>
         <td background="images/toplist-content.gif" style="border-left: 1px solid #CCCCCC;border-right: 1px solid #CCCCCC; background-repeat:repeat-x"> 
     <div style="line-height:20px">
-    <div style="padding-left:20px; padding-top:3px;"><strong>&raquo;XuanQuocQN@gmail.com</strong></div>
+    <div style="padding-left:5px; padding-top:5px; font-size: 10px;"><strong>&raquo;tuananhtour1310@gmail.com</strong></div>
 	
     </div>
     </td>
     <tr>
-    	<td height="30px" style="border-bottom: 1px solid #CCCCCC;border-left: 1px solid #CCCCCC;border-right: 1px solid #CCCCCC;">
+    	<td height="10px" style="border-bottom: 1px solid #CCCCCC;border-left: 1px solid #CCCCCC;border-right: 1px solid #CCCCCC;">
         	
       </td>
     </tr>
--- index.php
+++ index.php
@@ -28,7 +28,7 @@ echo "Tuan Anh Tourist"
 .mySlides {display:none;}
 
 </style>
-<body onload="kt()">
+<body onload="kt()" style = "background-image: url(backkkk.png);">
  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
@@ -37,7 +37,10 @@ echo "Tuan Anh Tourist"
 <script type="text/javascript" src="Scripts/tooltip/tip_centerwindow.js"></script> 
 <script type="text/javascript" src="Scripts/tooltip/tip_followscroll.js"></script>
 
-<div id="container">
+<div id="container" style = "-webkit-border-radius: 5px;
+border-radius: 5px; 
+-webkit-box-shadow: 0 0 30px -1px #BD0000;
+box-shadow: 0 0 30px -1px #BD0000;">
 <div>
 	<img src="background.png" style="max-width: 960px" alt="" />
 </div>
